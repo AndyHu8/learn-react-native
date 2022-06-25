a7708daf8a61541512e6e5bd299732df3d3c8a9b
Andy Hu <huandy01@gmail.com>
2022-06-25 18:26:19 +0200

    Lesson 6

--- testproject/App.js
+++ testproject/App.js
@@ -1,9 +1,10 @@
 import Lesson3 from './Lesson3';
 import Lesson4 from './Lesson4';
 import Lesson5 from './Lesson5';
+import Lesson6 from './Lesson6';
 
 export default function App() {
   return (
-    <Lesson5/>
+    <Lesson6/>
   );
 }
---
+++ testproject/Lesson6.jsx
@@ -0,0 +1,48 @@
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { useState } from 'react';
+
+//Lists & ScrollView
+export default function Lesson6() {
+const [people, setPeople] = useState([
+    {name: "andy", key: 1},
+    {name: "hu", key: 2},
+    {name: "xiaohu", key: 3},
+    {name: "andy", key: 1},
+    {name: "hu", key: 2},
+    {name: "xiaohu", key: 3},
+    {name: "andy", key: 1},
+    {name: "hu", key: 2},
+    {name: "xiaohu", key: 3}
+]);
+
+  return (
+    <View style={styles.container}>
+        <ScrollView>
+            {people.map(item => (
+                <View key={item.key}>
+                    <Text style={styles.item}>{item.name}</Text>
+                </View>
+                )
+            )}
+        </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+
+  item: {
+    marginTop: 24,
+    padding: 30,
+    fontSize: 24,
+    backgroundColor: "pink",
+  }
+});
